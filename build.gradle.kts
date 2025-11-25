d31dda685921b804936b5f4d70a3ceb4a1505491
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2025-11-25 18:10:44 +0900

    프로젝트 생성

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	war
+	id("org.springframework.boot") version "4.0.0"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.newsmoa"
+version = "0.0.1-SNAPSHOT"
+description = "news moas web app backend"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat-runtime")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
