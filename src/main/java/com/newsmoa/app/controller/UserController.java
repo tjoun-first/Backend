f864879178c792ee3bb19b91a866cad05de03052
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2025-12-01 15:27:51 +0900

    로그인 문제 해결

--- src/main/java/com/newsmoa/app/controller/UserController.java
+++ src/main/java/com/newsmoa/app/controller/UserController.java
@@ -1,28 +1,35 @@
 package com.newsmoa.app.controller;
 
+import com.newsmoa.app.dto.UserRequest;
+import com.newsmoa.app.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.newsmoa.app.dto.UserRequest;
-import com.newsmoa.app.service.UserService;
-
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping("/api")
-@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public UserController(UserService userService, AuthenticationManager authenticationManager) {
+        this.userService = userService;
+        this.authenticationManager = authenticationManager;
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody UserRequest userRequest) {
@@ -35,7 +42,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserRequest userRequest) {
+    public ResponseEntity<?> login(@RequestBody UserRequest userRequest, HttpServletRequest request) {
         try {
             // AuthenticationManager를 사용하여 인증 시도
             Authentication authentication = authenticationManager.authenticate(
@@ -43,8 +50,15 @@ public class UserController {
             );
 
             // SecurityContext에 인증 정보 저장
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            securityContext.setAuthentication(authentication);
 
+            HttpSession session = request.getSession(true);
+            session.setAttribute(
+                    HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                    securityContext
+            );
+            
             return ResponseEntity.ok("로그인 성공");
 
         } catch (Exception e) {
