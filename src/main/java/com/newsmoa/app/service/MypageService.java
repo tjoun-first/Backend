d92394c2db044578cc057c5431ecafa62d6e46c8
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2025-12-01 16:45:04 +0900

    컨트롤러 의존성 정리(마이페이지)

--- src/main/java/com/newsmoa/app/service/MypageService.java
+++ src/main/java/com/newsmoa/app/service/MypageService.java
@@ -1,6 +1,5 @@
 package com.newsmoa.app.service;
 
-import com.newsmoa.app.domain.Article;
 import com.newsmoa.app.domain.User;
 import com.newsmoa.app.domain.YourArticle;
 import com.newsmoa.app.dto.ArticleResponse;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
