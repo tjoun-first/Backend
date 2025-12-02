e3ee911475cbbbb3d9e6af544514df2b83dcfa2d
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2025-12-02 16:42:34 +0900

    단어 검색 api 추가

---
+++ src/main/java/com/newsmoa/app/controller/UtilController.java
@@ -0,0 +1,25 @@
+package com.newsmoa.app.controller;
+
+import com.newsmoa.app.util.AiUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/util")
+public class UtilController {
+    
+    private final AiUtil aiUtil;
+
+    @Autowired
+    public UtilController(AiUtil aiUtil) {
+        this.aiUtil = aiUtil;
+    }
+
+    @GetMapping("/word-meaning")
+    public String getWordMeaning(@RequestParam("word") String word, @RequestParam("sentence") String sentence){
+        return aiUtil.queryWord(word, sentence);
+    }
+}
